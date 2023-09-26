d00f978ba1ef05deff92e6a7e487a1179a9a1d53
Alexferraz2 <alex_ferraz2@hotmail.com>
2023-09-26 14:12:50 -0300

    Adcionado recurso de navegação suave ao clicar em ítem do menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var showNav = document.getElementById('menu-opener')
 var navshow = document.getElementById('display-nav')
+var closemenu = document.querySelectorAll('a')
 
 showNav.addEventListener("click", () => {
     navshow.classList.toggle('navshow')
